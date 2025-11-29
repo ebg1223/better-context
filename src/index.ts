0db1c34266052a420966254b60ca866fd991cef2
Ben Davis <davis.benjamin41902@gmail.com>
2025-11-28 20:39:38 -0500

    aight this seems like the right path...

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { BunRuntime } from "@effect/platform-bun";
-import { Effect } from "effect";
+import { Effect, Layer } from "effect";
+import { OcService } from "./services/oc";
+
+const programLayer = Layer.mergeAll(OcService.Default);
 
 const program = Effect.gen(function* () {
   yield* Effect.log("this will be interesting soon I think");
-});
+
+  const oc = yield* OcService;
+
+  yield* oc.testPrompting(
+    "How do I write a hello world program in elixir? Don't write it, just explain how to do it."
+  );
+}).pipe(
+  Effect.provide(programLayer),
+  Effect.catchAll((error) => {
+    console.error("Error:", error);
+    return Effect.void;
+  })
+);
 
 BunRuntime.runMain(program);
---
+++ src/services/oc.ts
@@ -0,0 +1,67 @@
+import { createOpencode } from "@opencode-ai/sdk";
+import { Effect } from "effect";
+import { TaggedError } from "effect/Data";
+
+class OcError extends TaggedError("OcError")<{
+  readonly message: string;
+  readonly cause?: unknown;
+}> {}
+
+const ocService = Effect.gen(function* () {
+  const { client, server } = yield* Effect.tryPromise({
+    try: () => createOpencode(),
+    catch: (err) =>
+      new OcError({ message: "FAILED TO CREATE OPENCODE CLIENT", cause: err }),
+  });
+
+  yield* Effect.addFinalizer(() =>
+    Effect.sync(() => {
+      console.log("CLOSING OPENCODE SERVER");
+      server.close();
+    })
+  );
+
+  return {
+    testPrompting: (prompt: string) =>
+      Effect.gen(function* () {
+        const session = yield* Effect.promise(() => client.session.create());
+
+        if (session.error) {
+          return yield* Effect.fail(
+            new OcError({
+              message: "FAILED TO START OPENCODE SESSION",
+              cause: session.error,
+            })
+          );
+        }
+
+        const resp = yield* Effect.promise(() => {
+          return client.session.prompt({
+            path: { id: session.data.id },
+            body: {
+              parts: [{ type: "text", text: prompt }],
+            },
+          });
+        });
+
+        if (resp.error) {
+          return yield* Effect.fail(
+            new OcError({
+              message: "FAILED TO TEST PROMPTING",
+              cause: resp.error,
+            })
+          );
+        }
+
+        resp.data.parts.map((part) => {
+          console.log(part);
+        });
+
+        return null;
+      }),
+  };
+});
+
+export class OcService extends Effect.Service<OcService>()("OcService", {
+  scoped: ocService,
+}) {}
